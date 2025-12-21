7137721a4f3f622407fb06cece9c37ea66be51df
aryanxengineer <aryanxengineer@gmail.com>
2025-12-21 08:21:44 +0530

    Learn't how socket and io works on server and client side understand the flow of events and how to manage the events on both side.

--- socketio/src/index.ts
+++ socketio/src/index.ts
@@ -3,15 +3,14 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 app.get("/", (req, res) => {
   res.render("index");
--- socketio/src/server.ts
+++ socketio/src/server.ts
@@ -1,16 +1,54 @@
-import app from "./index.js";
+// src/server.ts
 import { createServer } from "http";
 import { Server } from "socket.io";
+import app from "./index.js";
 
 const httpServer = createServer(app);
+
 const io = new Server(httpServer);
 
 io.on("connection", (socket) => {
-  console.log("a user connected");
+  console.log("✅ User connected:", socket.id);
+
+  // This is how we are creating rooms using socket io
+  socket.join("room1")
+  socket._cleanup()
+  socket._error('error related socket');
+  socket._onclose("forced close", 'client close this forcefully');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // This is all about the emiiting events from server side and how to emit and recieve event from client and how to learn broadcasting aswell.
+
+  // socket.on("client-message", (data) => {
+  //   console.log("📩 Message received:", data);
+
+  //   socket.emit("messageReceived", {
+  //     status: "ok",
+  //     text: data.message,
+  //   });
+  // });
+
+  // socket.on("typing", () => {
+  //   socket.broadcast.emit("typing");
+  // });
+
+  socket.on("disconnect", () => {
+    console.log("❌ User disconnected:", socket.id);
+  });
 });
 
-const PORT = process.env.PORT || 3000;
-
-httpServer.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+httpServer.listen(3000, () => {
+  console.log("🚀 Server running on http://localhost:3000");
 });
